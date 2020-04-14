4d4794b5ef2e2309b76e4c99d723dde0239ccb58
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2020-04-14 16:13:38 -0400

    graphs day 1 MVP and starting on day 2

---
+++ Breakoutroom.py
@@ -0,0 +1,39 @@
+# Write a function that takes a 2D binary array and returns the number of 1 islands. An island consists of 1s that are connected to the north, south, east or west. For example:
+
+def island_counter(self, grid):
+    if not grid:
+        return 0
+        
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '1':
+                self.dfs(grid, i, j)
+                count += 1
+    return count
+
+def dfs(self, grid, i, j):
+    if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+        return
+    grid[i][j] = '#'
+    self.dfs(grid, i+1, j)
+    self.dfs(grid, i-1, j)
+    self.dfs(grid, i, j+1)
+    self.dfs(grid, i, j-1)
+
+
+
+grid = [[0, 1, 0, 1, 0],
+           [1, 1, 0, 1, 1],
+           [0, 0, 1, 0, 0],
+           [1, 0, 1, 0, 0],
+           [1, 1, 0, 0, 0]]
+print (island_counter(grid, 'None')) # returns 4
+
+
+
+# islands consist of - connected components 
+# connected - neighbors (edges)
+# directions, nsew(edges)
+# 2d array - graph, more or less
+# returns (shape of solution) - number of islands
